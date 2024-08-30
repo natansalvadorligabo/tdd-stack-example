b2cbd5a24806b57501d10077834e0559bbe94613
natansalvadorligabo <salvadorligabo@gmail.com>
2024-08-30 15:13:43 -0300

    feat: second test

--- src/main/java/Pilha.java
+++ src/main/java/Pilha.java
@@ -1,10 +1,23 @@
 // faça o suficiente para o teste passar!!!
 public class Pilha {
+
+    private Object elemento;
+    private int quantidade = 0;
+
     public boolean estaVazia() {
-        return true;
+        return (elemento == null);
     }
 
     public int tamanho() {
-        return 0;
+        return quantidade;
+    }
+
+    public void empilha(int i) {
+        this.elemento = i;
+        quantidade++;
+    }
+
+    public Object topo() {
+        return elemento;
     }
 }
--- src/test/java/PilhaTest.java
+++ src/test/java/PilhaTest.java
@@ -12,4 +12,16 @@ public class PilhaTest {
         // o tamanho da pilha é igual a 0?
         assertEquals(0, p.tamanho());
     }
+
+    @Test
+    public void empilhaUmElemento() {
+        Pilha p = new Pilha();
+        p.empilha(5);
+        // a condicação é falsa?
+        assertFalse(p.estaVazia());
+        // o tamanho da pilha é igual a 1?
+        assertEquals(1, p.tamanho());
+        // o topo da pilha é igual a 5?
+        assertEquals(5, p.topo());
+    }
 }
